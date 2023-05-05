7a4b82c629d9a7f96981a6302f24da55f116bb9d
kyokoth <sirforfish@gmail.com>
2023-05-05 20:09:27 +0700

    upgrade functional

--- main.py
+++ main.py
@@ -1,46 +1,116 @@
 from os import getenv
 from time import sleep
 from dotenv import load_dotenv
+from hashlib import md5
+from json import dumps
 
+import pymongo
 from tomtom_map import TomtomAPI
 
-from origamibot import OrigamiBot
+from origamibot import OrigamiBot, types
 from origamibot.listener import Listener
-from origamibot.types import Message, InlineQuery, InputMessageContent, InlineQueryResultArticle, InlineQueryResultLocation
 
 class BotsCommands:
     def __init__(self, bot: OrigamiBot) -> None:
         self.bot = bot
     
-    def hello(self, message: Message) -> None:
-        print(message)
-        self.bot.send_message(message.chat.id, f'Привет {message.chat.first_name}')
+    def start(self, message: types.Message) -> None:
+        self.bot.send_message(message.chat.id, "Введите число!")
 
 class BotsInlines:
     def __init__(self, bot: OrigamiBot) -> None:
         self.bot = bot
 
-    def on_inline(self, inline: InlineQuery) -> None:
+    def inline_handler(self, inline_query: types.InlineQuery) -> None:
+        '''
+        global results
+        places = mapAPI.nearby_search(lat=inline_query.location.latitude,
+                                      lon=inline_query.location.longitude, 
+                                      lang='ru-RU',
+                                      query=inline_query.query)
+        
+        if places == None: return None
+
+        results = []
+
+        for place in places['results']:
+            for info_place in mapAPI.place_by_id(place['id'], language='ru-RU', mapcodes='Local', view='RU')['results']:
+                results.append({
+                    'id': place['id'],
+                    'name': info_place['poi']['name'],
+                    'phone': info_place['poi']['phone'] if 'phone' in info_place['poi'].keys() else None,
+                    'address': info_place['address']['freeformAddress'],
+                    'position': info_place['position'],
+                    'entryMainPoint': info_place['entryPoints'][0]['position'],
+                    'today_work_time': info_place['poi']['openingHours']['timeRanges'][0] if 'openingHours' in info_place['poi'].keys() else None
+                })
+
         inlines = []
 
-        places = mapAPI.nearby_search(inline.location.latitude, inline.location.longitude, "ru-RU", inline.query)
+        for place in results:
+            inlines.append(types.InlineQueryResultLocation(place['id'], place['entryMainPoint']['lat'], place['entryMainPoint']['lon'], place['name']))
+
+        self.bot.answer_inline_query(inline_query.id, inlines, cache_time=1)
+        '''
+        #print(result)
+        
+        #with open('places.json', 'w', encoding='utf-8') as f:
+        #    f.write(dumps(places, ensure_ascii=False, indent = 4))
+
+        '''
+        text = inline_query.query or 'echo'
+        input_content = types.InputTextMessageContent(f'<b>{text}</b> - {user_data}', parse_mode='html')
+        result_id: str = md5(text.encode()).hexdigest()
+
+        item = types.InlineQueryResultArticle(
+            id=result_id,
+            title='hi',
+            input_message_content=input_content,
+            description='hi'
+        )
+        
+        self.bot.answer_inline_query(inline_query_id=inline_query.id,
+                                     results=[item],
+                                     cache_time=1)
+        '''
+
+        inlines = []
+
+        places = mapAPI.nearby_search(inline_query.location.latitude, inline_query.location.longitude, 'ru-RU', inline_query.query.capitalize())
 
         if places == None:
             return None
 
-        for place in places["results"]:
-            inlines.append(InlineQueryResultLocation(place["id"], place["position"]["lat"], place["position"]["lon"], place["poi"]["name"]))
+        for place in places['results']:
+            inlines.append(types.InlineQueryResultLocation(place['id'], place['position']['lat'], place['position']['lon'], place['poi']['name']))
 
-        self.bot.answer_inline_query(inline.id, inlines)
+        self.bot.answer_inline_query(inline_query.id, inlines)
+
+class MessageListener(Listener):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+    
+    def on_message(self, message: types.Message) -> None:
+        if message.via_bot != None and message.location != None:
+            #print(self.bot.updates[0].chosen_inline_result)
+            #print(self.bot.updates[0].chosen_inline_result.result_id)
+            #print(mapAPI.reverse_geocode(message.location.latitude, message.location.longitude))
+            print(mapAPI.place_by_id(self.bot.updates[0].chosen_inline_result.result_id, language='ru-RU', mapcodes='Local', view='RU'))
+            
 
 if __name__ == '__main__':
     load_dotenv()
     token = getenv('TOKEN')
-    token_map = getenv('TOKENMAP')
+    token_map = getenv('TOMTOM')
+    token_map_details = getenv('GEOAPIFY_PDA')
 
     bot = OrigamiBot(token)
-    mapAPI = TomtomAPI(token_map)
+    mapAPI = TomtomAPI(token_map, token_map_details)
+    database = pymongo.MongoClient("mongodb://localhost:27017/")['geobot']
 
+    user_data = ''
+
+    bot.add_listener(MessageListener(bot))
     bot.add_inline(BotsInlines(bot))
     bot.add_commands(BotsCommands(bot))
 
--- tomtom_map.py
+++ tomtom_map.py
@@ -1,44 +1,79 @@
 import requests
 
 class TomtomAPI:
-    def __init__(self, key: str) -> None:
-        self.key = key
-        self.base_link = "https://api.tomtom.com/search/2/"
+    def __init__(self, key_tomtom: str, key_geoapify: str) -> None:
+        self.key_tomtom = key_tomtom
+        self.key_geoapify = key_geoapify
+        self.base_link_tomtom = "https://api.tomtom.com/search/2/"
+        self.base_link_geoapify = "https://api.geoapify.com/v2/"
     
-    def reverse_geocode(self, lat: float, lon: float):
+    def reverse_geocode(self, lat: str, lon: str):
         '''
         Converts the latitude and longitude into information about a place
         '''
-        link = f"{self.base_link}reverseGeocode/{lat},{lon}.json?key={self.key}&radius=100"
+        link = f"{self.base_link_tomtom}reverseGeocode/{lat},{lon}.json?key={self.key_tomtom}"
         
         response = requests.get(link)
         
         return response.json()
     
-    def nearby_search(self, lat: float, lon: float, lang: str, place: str):
+    def nearby_search(self, lat: str, lon: str, lang: str, query: str):
         '''
         Finds nearby places based on latitude, longitude, language, and place type
         '''
-        place_code = self._find_place_code(lang, place)
+        place_code = self._find_place_code(lang, query.capitalize())
         
         if place_code == None:
             return None
         
-        places_link = f"{self.base_link}nearbySearch/.json?key={self.key}&lat={lat}&lon={lon}&language={lang}&categorySet={place_code}"
+        places_link = f"{self.base_link_tomtom}nearbySearch/.json?key={self.key_tomtom}&lat={lat}&lon={lon}&language={lang}&categorySet={place_code}"
 
         places_response = requests.get(places_link)
         
         return places_response.json()
 
-    def _find_place_code(self, lang: str, place: str):
+    def place_details(self, lat: str, lon: str, lang: str):
+        place_link = f"{self.base_link_geoapify}place-details?lat={lat}&lon={lon}&lang={lang}&apiKey={self.key_geoapify}"
+
+        place_response = requests.get(place_link)
+
+        return place_response.json()
+
+    def place_by_id(self,
+                    entityId: str,
+                    language: str = 'en-US',
+                    openingHours: str = 'nextSevenDays',
+                    timeZone: str = 'iana',
+                    mapcodes: str = 'Local,Alternative,International',
+                    relatedPois: str = 'off',
+                    view: str = 'Unified') -> list:
+        
+        link = f"{self.base_link_tomtom}place.json"
+
+        response = requests.get(link, params={
+            "entityId": entityId,
+            "key": self.key_tomtom,
+            "language": language,
+            "openingHours": openingHours,
+            "timeZone": timeZone,
+            "mapcodes": mapcodes,
+            "relatedPois": relatedPois,
+            "view": view
+        })
+
+        return response.json()
+
+
+
+    def _find_place_code(self, lang: str, query: str):
         '''
         Obtains a place code based on place type and language
         '''
-        categories_link = f"{self.base_link}poiCategories.json?key={self.key}&language={lang}"
+        categories_link = f"{self.base_link_tomtom}poiCategories.json?key={self.key_tomtom}&language={lang}"
 
         categories_response = requests.get(categories_link)
 
-        for category in categories_response.json()["poiCategories"]:
-            if category['name'] == place or place in category['synonyms']:
+        for category in categories_response.json()['poiCategories']:
+            if category['name'] == query or query in category['synonyms']:
                 return category['id']
         return None
