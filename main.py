fabb953e2ee45a816d2fa9bbbdd75522d2e467c9
kyokoth <sirforfish@gmail.com>
2023-05-12 05:29:56 +0700

    again some updates

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from os import getenv
 from time import sleep
 from dotenv import load_dotenv
-from hashlib import md5
-from json import dumps
+from pytz import timezone
+from datetime import datetime
+from timezonefinder import TimezoneFinder
+from math import floor
 
 import pymongo
 from tomtom_map import TomtomAPI
@@ -15,65 +17,16 @@ class BotsCommands:
         self.bot = bot
     
     def start(self, message: types.Message) -> None:
-        self.bot.send_message(message.chat.id, "Введите число!")
+        self.bot.send_message(message.chat.id, "Привет, я Геобот! Чтобы начать работу введите @ggeogeobot и любое желаемое место, например:\n@ggeogeobot кафе")
 
 class BotsInlines:
     def __init__(self, bot: OrigamiBot) -> None:
         self.bot = bot
 
     def inline_handler(self, inline_query: types.InlineQuery) -> None:
-        '''
-        global results
-        places = mapAPI.nearby_search(lat=inline_query.location.latitude,
-                                      lon=inline_query.location.longitude, 
-                                      lang='ru-RU',
-                                      query=inline_query.query)
+        if inline_query.location == None:
+            return None
         
-        if places == None: return None
-
-        results = []
-
-        for place in places['results']:
-            for info_place in mapAPI.place_by_id(place['id'], language='ru-RU', mapcodes='Local', view='RU')['results']:
-                results.append({
-                    'id': place['id'],
-                    'name': info_place['poi']['name'],
-                    'phone': info_place['poi']['phone'] if 'phone' in info_place['poi'].keys() else None,
-                    'address': info_place['address']['freeformAddress'],
-                    'position': info_place['position'],
-                    'entryMainPoint': info_place['entryPoints'][0]['position'],
-                    'today_work_time': info_place['poi']['openingHours']['timeRanges'][0] if 'openingHours' in info_place['poi'].keys() else None
-                })
-
-        inlines = []
-
-        for place in results:
-            inlines.append(types.InlineQueryResultLocation(place['id'], place['entryMainPoint']['lat'], place['entryMainPoint']['lon'], place['name']))
-
-        self.bot.answer_inline_query(inline_query.id, inlines, cache_time=1)
-        '''
-        #print(result)
-        
-        #with open('places.json', 'w', encoding='utf-8') as f:
-        #    f.write(dumps(places, ensure_ascii=False, indent = 4))
-
-        '''
-        text = inline_query.query or 'echo'
-        input_content = types.InputTextMessageContent(f'<b>{text}</b> - {user_data}', parse_mode='html')
-        result_id: str = md5(text.encode()).hexdigest()
-
-        item = types.InlineQueryResultArticle(
-            id=result_id,
-            title='hi',
-            input_message_content=input_content,
-            description='hi'
-        )
-        
-        self.bot.answer_inline_query(inline_query_id=inline_query.id,
-                                     results=[item],
-                                     cache_time=1)
-        '''
-
         inlines = []
 
         places = mapAPI.nearby_search(inline_query.location.latitude, inline_query.location.longitude, 'ru-RU', inline_query.query.capitalize())
@@ -82,7 +35,7 @@ class BotsInlines:
             return None
 
         for place in places['results']:
-            inlines.append(types.InlineQueryResultLocation(place['id'], place['position']['lat'], place['position']['lon'], place['poi']['name']))
+            inlines.append(types.InlineQueryResultLocation(place['id'], place['position']['lat'], place['position']['lon'], f"{place['poi']['name']} в {round(place['dist'])} метрах от вас"))#, input_message_content=types.InputTextMessageContent(f"В {round(place['dist'])} метрах от вас")))
 
         self.bot.answer_inline_query(inline_query.id, inlines)
 
@@ -92,10 +45,72 @@ class MessageListener(Listener):
     
     def on_message(self, message: types.Message) -> None:
         if message.via_bot != None and message.location != None:
-            #print(self.bot.updates[0].chosen_inline_result)
-            #print(self.bot.updates[0].chosen_inline_result.result_id)
-            #print(mapAPI.reverse_geocode(message.location.latitude, message.location.longitude))
-            print(mapAPI.place_by_id(self.bot.updates[0].chosen_inline_result.result_id, language='ru-RU', mapcodes='Local', view='RU'))
+            place = mapAPI.place_by_id(self.bot.updates[0].chosen_inline_result.result_id, language='ru-RU', mapcodes='Local', view='RU')['results'][0]
+
+            result = {
+                'id': place['id'],
+                'name': place['poi']['name'],
+                'phone': place['poi']['phone'] if 'phone' in place['poi'].keys() else None,
+                'address': place['address']['freeformAddress'],
+                'position': place['position'],
+                'entryMainPoint': place['entryPoints'][0]['position'],
+                'today_work_time': place['poi']['openingHours']['timeRanges'][0] if 'openingHours' in place['poi'].keys() else None
+            }
+
+            text = ""
+
+            text += f"{result['name']}\n"
+            text += f"{result['phone']}\n" if result['phone'] != None else ''
+
+            if result['today_work_time'] != None:
+                text += f"Время работы: {''.join(('{:>02}'.format(str(result['today_work_time']['startTime']['hour'])), ':', '{:>02}'.format(str(result['today_work_time']['startTime']['minute'])), ' - ', '{:>02}'.format(str(result['today_work_time']['endTime']['hour'])), ':', '{:>02}'.format(str(result['today_work_time']['endTime']['minute']))))}\n"
+                
+                time_at_place_now = datetime.now(timezone(timefinder.timezone_at(lng=result['position']['lon'], lat=result['position']['lat']))).time()
+                time_start = datetime.strptime(''.join((str(result['today_work_time']['startTime']['hour']), ':', str(result['today_work_time']['startTime']['minute']))), '%H:%M').time()
+                time_end = datetime.strptime(''.join((str(result['today_work_time']['endTime']['hour']), ':', str(result['today_work_time']['endTime']['minute']))), '%H:%M').time()
+
+                if time_at_place_now.hour == time_start.hour:
+                    if time_at_place_now.minute == time_start.minute:
+                        text += f'Заведение недавно открылось'
+                    elif time_at_place_now.minute < time_start.minute:
+                        text += f'До открытия осталось {floor((time_start.minute*60 - (time_at_place_now.minute*60 + time_at_place_now.second)) / 60)} минут\n'
+                    else:
+                        text += f'До открытия осталось {floor(((time_at_place_now.minute*60 + time_at_place_now.second) - time_start.minute*60) / 60)} минут\n'
+                
+                elif time_at_place_now.hour == time_end.hour:
+                    if time_at_place_now.minute == time_end.minute:
+                        text += f'Заведение недавно закрылось'
+                    elif time_at_place_now.minute < time_end.minute:
+                        text += f'До закрытия осталось {floor((time_end.minute*60 - (time_at_place_now.minute*60 + time_at_place_now.second)) / 60)} минут\n'
+                    else:
+                        text += f'До закрытия осталось {floor(((time_at_place_now.minute*60 + time_at_place_now.second) - time_end.minute*60) / 60)} минут\n'
+                
+                elif time_at_place_now.hour < time_start.hour:
+                    text += f'До открытия осталось '
+                    hours = floor(((time_start.hour*3600 + time_start.minute*60) - (time_at_place_now.hour*3600 + time_at_place_now.minute*60 + time_at_place_now.second)) / 3600)
+                    if time_at_place_now.minute == time_start.minute:
+                        text += f'{hours} часа'
+                    elif time_at_place_now.minute < time_start.minute:
+                        text += f'{hours} часа и {floor((time_start.minute*60 - (time_at_place_now.minute*60 + time_at_place_now.second)) / 60)} минут'
+                    else:
+                        text += f'{hours} часа и {floor(((time_at_place_now.minute*60 + time_at_place_now.second) - time_start.minute*60) / 60)} минут'
+                    text += '\n'
+
+                elif time_at_place_now.hour < time_end.hour:
+                    text += f'До закрытия осталось '
+                    hours = floor(((time_end.hour*3600 + time_end.minute*60) - (time_at_place_now.hour*3600 + time_at_place_now.minute*60 + time_at_place_now.second)) / 3600)
+                    if time_at_place_now.minute == time_end.minute:
+                        text += f'{hours} часа'
+                    elif time_at_place_now.minute < time_end.minute:
+                        text += f'{hours} часа и {floor((time_end.minute*60 - (time_at_place_now.minute*60 + time_at_place_now.second)) / 60)} минут'
+                    else:
+                        text += f'{hours} часа и {floor(((time_at_place_now.minute*60 + time_at_place_now.second) - time_end.minute*60) / 60)} минут'
+                    text += '\n'
+
+            text += f"Адрес: {result['address']}"
+            
+            bot.send_message(chat_id=message.chat.id,
+                             text=text)
             
 
 if __name__ == '__main__':
@@ -113,6 +128,8 @@ if __name__ == '__main__':
     bot.add_listener(MessageListener(bot))
     bot.add_inline(BotsInlines(bot))
     bot.add_commands(BotsCommands(bot))
+
+    timefinder = TimezoneFinder()
 
     bot.start()
     while True:
